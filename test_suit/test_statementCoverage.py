f4efd5d6ec0acb6ab7c8fc038ea21444ecb3bc9f
Deepti Guntur <deepti25.guntur@gmail.com>
2023-10-26 16:09:38 -0400

    Test suites for statement and decision coverage

---
+++ test_suit/test_statementCoverage.py
@@ -0,0 +1,38 @@
+import unittest
+from isTriangle import Triangle
+# import sys
+# sys.path.append("..")
+# from src.Triangle import Triangle
+
+class TriangleTest(unittest.TestCase):
+    def test1(self):
+        actual = Triangle.classify(10, 10, 10)
+        expected = Triangle.Type.EQUILATERAL
+        self.assertEqual(actual, expected)
+    def test2(self):
+        actual = Triangle.classify(5, 10, 10)
+        expected = Triangle.Type.ISOSCELES
+        self.assertEqual(actual, expected)
+    def test3(self):
+        actual = Triangle.classify(10, 10, 5)
+        expected = Triangle.Type.ISOSCELES
+        self.assertEqual(actual, expected)
+    def test4(self):
+        actual = Triangle.classify(10, 5, 10)
+        expected = Triangle.Type.ISOSCELES
+        self.assertEqual(actual, expected)
+    def test5(self):
+        actual = Triangle.classify(10, 11, 12)
+        expected = Triangle.Type.SCALENE
+        self.assertEqual(actual, expected)
+    def test5(self):
+        actual = Triangle.classify(-1, 0, 1)
+        expected = Triangle.Type.INVALID
+        self.assertEqual(actual, expected)
+    def test6(self):
+        actual = Triangle.classify(4, 30, 100)
+        expected = Triangle.Type.INVALID
+        self.assertEqual(actual, expected)
+
+if __name__ == '__main__':
+    unittest.main()
